ad7b42d9bf8821c12a85bfccea509f2103a6bf86
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-06-04 14:06:51 +0530

    writing simple tests for my routes

---
+++ 3-vitest-test/src/tests/index.test.ts
@@ -0,0 +1,27 @@
+import {describe, expect, test ,it} from 'vitest';
+ import request from 'supertest';
+ import{ app }from '../index';
+
+
+ describe("POST /sum", () => {
+    it("should return the sum of two numbers", async () => {
+      const response = await request(app).post("/sum").send({
+         a: 1,
+         b: 2
+         });
+
+      expect(response.status).toBe(200);
+      expect(response.body.answer).toBe(3);
+    });
+
+    it("should return 411 when invalid input", async () => {
+        const response = await request(app).post("/sum").send({
+            a: 1,
+            b: "2"
+        });
+        expect(response.status).toBe(411);
+        expect(response.body.message).toBe("invalid input");
+    });
+
+   
+  });
