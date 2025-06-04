90bfbdb722b2d1f852aaa2d8a7c45d8ab5e81767
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-06-04 16:06:31 +0530

    adding mocking in it which basically means faking db connections for unit testing

--- 3-vitest-test/src/tests/index.test.ts
+++ 3-vitest-test/src/tests/index.test.ts
@@ -1,7 +1,17 @@
-import {describe, expect ,it} from 'vitest';
+import {describe, expect ,it,vi} from 'vitest';
  import request from 'supertest';
  import{ app }from '../index';
 
+
+// vi.mock("../db", () => ({
+//     prisma: {
+//         sum: {
+//             create: vi.fn(),
+//         },
+//     },
+// }));
+
+vi.mock("../db");
 
  describe("POST /sum", () => {
     it("should return the sum of two numbers", async () => {
