a4a99044cc9c5b342f796230a2d7c00c214ce4ac
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-06-04 13:48:54 +0530

    slightly complex route

--- 2-express-app/src/tests/index.test.ts
+++ 2-express-app/src/tests/index.test.ts
@@ -10,8 +10,33 @@ import {describe, expect, it} from '@jest/globals'
          a: 1,
          b: 2
          });
-         
+
       expect(response.status).toBe(200);
       expect(response.body.answer).toBe(3);
     });
+
+   
+  });
+
+  describe("GET /sum", () => {
+    it("should return the sum of two numbers", async () => {
+      const response = await request(app).get("/sum").query({
+         a: 12,
+         b: 223
+         });
+
+      expect(response.status).toBe(200);
+      expect(response.body.answer).toBe(235);
+    });
+
+     // this is for header test 
+    it ("should return the sum of negative numbers", async () => {
+        const response = await request(app).get("/sum").set({
+           a: "1",
+           b: "-2"
+           }).send();
+  
+        expect(response.status).toBe(200);
+        expect(response.body.answer).toBe(-3);
+      })
   });
