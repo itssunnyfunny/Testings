c1b3a3b4c57155e832d71d3bd8125fa7c81dad36
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-06-06 10:34:15 +0530

    doing spying on the mocked database calls and how to do it

--- 3-vitest-test/src/tests/index.test.ts
+++ 3-vitest-test/src/tests/index.test.ts
@@ -2,6 +2,7 @@ import {describe, expect ,it,vi} from 'vitest';
  import request from 'supertest';
  import{ app }from '../index';
 import { prisma } from '../__mock__/db';
+import { a } from 'vitest/dist/chunks/suite.d.FvehnV49';
 
 
 // vi.mock("../db", () => ({
@@ -24,10 +25,24 @@ vi.mock("../db");
         a: 1,
         b: 2
       });
+
+      // spying on the mocked function
+       vi.spyOn(prisma.sum, "create");
+
       const response = await request(app).post("/sum").send({
          a: 1,
          b: 2
          });
+
+      expect(prisma.sum.create).toHaveBeenCalledTimes(1);
+      expect(prisma.sum.create).toHaveBeenCalledWith({
+        data: {
+          a: 1,
+          b: 2,
+          type: "sum",
+          answer: 3
+        },
+      });
 
       expect(response.status).toBe(200);
       expect(response.body.id).toBe(1);
